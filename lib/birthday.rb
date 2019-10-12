ef5290ee118e1d46521997a7d0a9343f4ec9f4be
Victoria Hale <vic.hale74@gmail.com>
2019-10-12 18:41:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,4 +11,12 @@ def happy_birthday(birthday_kids)
  end
 end
 
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if kids_name <= 12 
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
+end
+
 
